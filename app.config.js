023dfc9e8e753e6ad8f1ec5c01ee103144e094be
chenwq@idea-group.cn <526728980@qq.com>
2022-09-14 09:51:23 +0800

    init

---
+++ app.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  db: {
+    appId: 'A6192942337552',
+    appKey: 'C1856A8E-B4C6-5BC5-6CCF-33D1BF8F2207'
+  },
+  cdn: {
+    host: 'http://p4t4vcu10.bkt.clouddn.com/',
+    bucket: 'vue-study',
+    ak: 'GYgaJ3VBYjcfXqA32hZd2r9rrRvAaP_9jfEPhE5n',
+    sk: 'X2ZMFccy_viLkHXBFB7TlDyP0QmuCIBQzTJZC6iJ'
+  }
+}
